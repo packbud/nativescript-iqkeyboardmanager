47b4a96dea31998b5972fcf54bb0217625410023
Roman L <raman@cmstuning.net>
2017-11-30 15:35:40 +0300

    Update declarations

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,11 @@
+declare const enum IQPreviousNextDisplayMode {
+  Default = 0,
+  AlwaysHide = 1,
+  AlwaysShow = 2
+}
+
 declare class IQKeyboardManager extends NSObject {
   enable: boolean;
-
+  previousNextDisplayMode: IQPreviousNextDisplayMode;
   static sharedManager(): IQKeyboardManager;
 }
